9264270b20bee5fd70d4b37d46c49c0495fa13f9
Catherine Bronzo <cbronzo@mac.com>
2019-03-23 03:06:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,5 +5,7 @@ let index = 0
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.addEventListener('keydown', function(e) {return onKeyDownHandler(e)
+    
+  })
 }
